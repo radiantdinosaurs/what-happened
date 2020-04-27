c7d6b0d979abe597d0df94e37a83449d44ce5f48
radiantdinosaurs <radiantdinosaurs@gmail.com>
2020-04-26 23:26:14 -0700

    Updates packages and cleans up code
    - Updates packages
    - Removes unused imports
    - Cleans up old code with new fetching logic
    - Removes logic for finding facts about the year, leaving just searching for facts about the day
    - Updates the README with a URL for the API
    - Other minor housekeeping

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const Client = require('node-rest-client').Client;
 const port = process.env.PORT || 8000;
 const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({defaultLayout: 'main'});
+const hbs = exphbs.create({ defaultLayout: 'main' });
 const bodyParser = require('body-parser');
-const client = new Client();
-const router = express.Router();
-const async = require('async');
 const path = require('path');
+const fetch = require('node-fetch');
 
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
@@ -16,61 +13,50 @@ app.use(express.static(port + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.listen(port);
-process.setMaxListeners(20);
 
-app.get('/', function(req, res) {
-    res.render('index', {
-        title: 'What Happened?'
-        });
+const API_BASE = (day, month) => `http://numbersapi.com/${day}/${month}/date`
+const PAGE_TITLE = 'What Happened?'
+
+app.get('/', function (request, response) {
+    response.render('index', {
+        title: PAGE_TITLE
+    });
 });
 
-app.post('/', function(req, res) {
-    const date = req.body.date;
-    if(isValidDate(date)) {
-        const monthArr = date.split('/');
-        const args = {path: {'month': monthArr[0], 'day': monthArr[1], 'year': monthArr[2]}};
-        async.waterfall([
-            function(callback) {
-                client.get('http://numbersapi.com/${month}/${day}/date', args, function(request, response) {
-                    callback(null, request);
-                }).on('error', function (err) {
-                    console.log('Something went wrong on the request', err.malUserData.options);
+app.post('/', function (request, response) {
+    const date = request.body.date;
+    if (testIsValidInput(date)) {
+        const dateArray = date.split('/');
+        const day = dateArray[0];
+        const month = dateArray[1];
+
+        fetch(API_BASE(day, month))
+            .then((response) => response.text())
+            .then((dateFact) => {
+                response.render('search-result', {
+                    title: PAGE_TITLE,
+                    msg: dateFact
                 });
-                client.on('error', function (err) {
-                    console.log('Something went wrong on the client', err);
-                });
-            },
-            function(monthDayFact, callback) {
-                client.get('http://numbersapi.com/${year}/year', args, function(request, response) {
-                    callback(null, monthDayFact, request);
-                }).on('error', function (err) {
-                    console.log('Something went wrong on the request', err.malUserData.options);
-                });
-                client.on('error', function (err) {
-                    console.log('Something went wrong on the client', err);
-                });
-            },
-            function(monthDayFact, yearFact, callback) {
-                res.render('search-result', {
-                    title: "What Happened on " + monthArr[0] + "/" + monthArr[1] + "/" + monthArr[2] + "?",
-                    msg: monthDayFact,
-                    year: yearFact
-                });
-                callback(null, 'done');
-            }
-        ], function (err, result) {
-        });
+            })
+            .catch((error) => {
+                console.log('Whoops, got an error: ', error)
+            })
     }
     else {
-        res.render('search-result', {
+        const invalidDate = request.body.date;
+        response.render('search-result', {
             title: "Oops!",
-            msg: "\"" + req.body.date + "\" is not a valid date!",
-            year: "Please try again, and put your number in the format MM/DD/YYYY"
+            msg: `${invalidDate} is not a valid date!`,
+            year: "Please try again, and put your number in the format MM/DD"
         });
     }
 });
 
-function isValidDate(date) {
-    const date_regex = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/ ;
-    return date_regex.test(date);
+/**
+ * Tests if the input is in the format of MM/DD
+ * @param {String} date - Expected to be MM/DD
+ */
+const testIsValidInput = (date) => {
+    const validDayAndMonth = /^((0[1-9])|(1[0-2]))\/(\d{2})$/;
+    return validDayAndMonth.test(date);
 }
